3a3b61ba8bf4ab150c70945578b67bc9f6441f99
FedeCB18 <127358667+FedeCB18@users.noreply.github.com>
2023-03-08 20:22:18 -0500

    Update Lab 6 (Comments)
    Comments

--- Lab 6.py
+++ Lab 6.py
@@ -1,6 +1,9 @@
+# Import used for array function
 import numpy as np
 
-
+# Convert number string into malleable array
+# Encode Array
+# Convert array back into number string
 def encode(num):
     lis = [int(x) for x in str(num)]
     arr = np.array(lis)
@@ -8,6 +11,8 @@ def encode(num):
     res = ''.join(map(str, val))
     return res
 
+# Convert number string into malleable array
+# Decode Array
 def decode(password:list):
     decode = ''
     for i in password:
@@ -18,20 +23,30 @@ def decode(password:list):
         decode += str(i)
     return decode
 
+# Main Code
 if __name__ == "__main__":
+    # Loop Function to keep program active unless option 3 is called.
     while True:
+        # Menu Options & Input Value for Menu Options
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
         inpt = int(input('\nPlease enter an option: '))
 
+        # Backend Menu Structure
         if inpt == 1:
+            # Input Value for password
+            # Encode Funtion
+            # Print Output Statement
             numb = int(input('Please enter your password to encode: '))
             num = encode(numb)
             print('You password has been encoded and stored!')
         elif inpt == 2:
+            # Try & Except function used in case option 2 is called before option 1
+            # Print Output statement
             try: 
                  print("The encoded password is", encode(numb), end=',')
                  print(' and the original password is', str(numb), end='.\n\n')
             except:
                  print("Please encode a password first before decoding.")
         elif inpt == 3:
+            # End Program
             exit()
